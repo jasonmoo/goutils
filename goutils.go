0d5f5f068d87c5334f3987a11be07903873ace88
Jason Mooberry <jason@vimeo.com>
2013-04-24 18:16:06 -0400

    bit of refactor and adding DiffBytes

--- goutils.go
+++ goutils.go
@@ -10,17 +10,33 @@ var (
 )
 
 // writes zeroes to every byte in []byte
-func Zero(b []byte) {
+func ZeroBytes(b []byte) {
 	for i, l := 0, len(b); i < l; i++ {
 		b[i] = byte(0)
 	}
 }
 // writes incrementing short as byte to []byte 0-255,0-255
-func Fill(b []byte) {
+func FillBytes(b []byte) {
 	for i, l := 0, len(b); i < l; i++ {
 		b[i] = byte(i)
 	}
 }
+func DiffBytes(a, b []byte) int {
+	ct, l := 0, 0
+	if len(a) < len(b) {
+		l = len(a)
+	} else {
+		l = len(b)
+	}
+	for i := 0; i < l; i++ {
+		if a[i] != b[i] {
+			ct++
+		}
+	}
+	return ct
+}
+
+
 // takes a number of bytes and outputs it in human readable format
 // ie: 1024 -> 1KB
 func ByteSizeToHumanReadable(b uint64, precision int) string {
--- goutils_test.go
+++ goutils_test.go
@@ -1,53 +1,65 @@
 package goutils
 
 import (
-	"bytes"
 	"testing"
 )
 
-func TestZero(t *testing.T) {
+const (
+	onemb int = 1 << 20
+)
+
+type diffcase struct{
+	n int
+	a, b []byte
+}
+
+func TestDiffBytes(t *testing.T) {
 
 	// safe to run in parallel
 	t.Parallel()
 
-	// test 1B - 1M, in powers of 2
-	for i, b := 1, 1 << 20; i < b; i<<=1 {
-		blank, tozero := make([]byte, i), make([]byte, i)
-		Zero(tozero)
-		if !bytes.Equal(blank, tozero) {
-			var ct int
-			for ct, ii := 0, 0; i < ii; ii++ {
-				if blank[ii] != tozero[ii] {
-					ct++
-				}
-			}
-			t.Errorf("[]byte arrays are different.  Found %d non-matching bytes.",ct)
+	cases := []diffcase{
+		{0, []byte{0}, []byte{0}},
+		{3, []byte{0,0,0}, []byte{1,1,1}},
+		{1, []byte{0,1,1}, []byte{1,1,1}},
+		{1, []byte{0}, []byte{1}},
+	}
+
+	for _, c := range cases {
+		if d := DiffBytes(c.a, c.b); d != c.n {
+			t.Errorf("Diff produced %d, expected %d, (%v,%v)", d, c.n, c.a, c.b)
 		}
 	}
 
 }
 
-func TestFill(t *testing.T) {
+func TestZeroBytes(t *testing.T) {
 
 	// safe to run in parallel
 	t.Parallel()
 
-	// test 1B - 1M, in powers of 2
-	for i, b, diff_ct := 1, 1 << 20, 0; i < b; i<<=1 {
-		tofill := make([]byte, i)
-		Fill(tofill)
-		for i, b := range tofill {
-			if byte(i) != b {
-				diff_ct++
-			}
-		}
-		if diff_ct > 0 {
-			t.Errorf("[]byte arrays are different.  Found %d non-matching bytes.", diff_ct)
-		}
-		diff_ct = 0
+	aa, bb := make([]byte, onemb), make([]byte, onemb)
+	ZeroBytes(bb)
+	if ct := DiffBytes(aa, bb); ct > 0 {
+		t.Errorf("[]byte arrays are different.  Found %d non-matching bytes.", ct)
 	}
 
 }
+
+func TestFillBytes(t *testing.T) {
+
+	// safe to run in parallel
+	t.Parallel()
+
+	a, b := make([]byte, onemb), make([]byte, onemb)
+	FillBytes(a)
+	FillBytes(b)
+	if ct := DiffBytes(a, b); ct > 0 {
+		t.Errorf("[]byte arrays are different.  Found %d non-matching bytes.", ct)
+	}
+
+}
+
 
 func TestByteSizeToHumanReadable(t *testing.T) {
 
@@ -58,7 +70,7 @@ func TestByteSizeToHumanReadable(t *testing.T) {
 		uint64(0): "0B",
 		uint64(13): "13B",
 		uint64(1 << 10): "1KB",
-		uint64(1 << 20): "1MB",
+		uint64(onemb): "1MB",
 		uint64(1 << 30): "1GB",
 		uint64(1 << 40): "1TB",
 		uint64(1 << 50): "1PB",
@@ -89,7 +101,7 @@ func TestHumanReadableSizeToBytes(t *testing.T) {
 		"0B": uint64(0),
 		"13B": uint64(13),
 		"1KB": uint64(1 << 10),
-		"1MB": uint64(1 << 20),
+		"1MB": uint64(onemb),
 		"1GB": uint64(1 << 30),
 		"1TB": uint64(1 << 40),
 		"1PB": uint64(1 << 50),
@@ -135,26 +147,36 @@ func TestIsPowerOf2(t *testing.T) {
 
 
 
-
-func BenchmarkZero(b *testing.B) {
+func BenchmarkZeroBytes(b *testing.B) {
 
 	b.StopTimer()
-	buf, size := make([]byte, 1 << 20), 1 << 20
+	buf, size := make([]byte, onemb), onemb
 	b.SetBytes(int64(size * b.N))
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		Zero(buf)
+		ZeroBytes(buf)
 	}
 
 }
-func BenchmarkFill(b *testing.B) {
+func BenchmarkFillBytes(b *testing.B) {
 
 	b.StopTimer()
-	buf, size := make([]byte, 1 << 20), 1 << 20
-	b.SetBytes(int64(size * b.N))
+	buf := make([]byte, onemb)
+	b.SetBytes(int64(onemb * b.N))
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		Fill(buf)
+		FillBytes(buf)
+	}
+
+}
+func BenchmarkDiffBytes(b *testing.B) {
+
+	b.StopTimer()
+	aa, bb := make([]byte, onemb), make([]byte, onemb)
+	b.SetBytes(int64(onemb * b.N))
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		DiffBytes(aa,bb)
 	}
 
 }
